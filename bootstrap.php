7cb6e1d036419efa1384602b270f7668ec758840
廖文忠 <wcliao1962@gmail.com>
2018-10-23 17:57:54 +0800

    project init; db setting

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 33060);
+define('DB_DATABASE', 'blog_0804');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
