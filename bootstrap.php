ff26eb6c9acf83281fa1f8bb5eb151399a9955fa
Liao Wen Chung <wcliao1962@gmail.com>
2020-09-29 23:18:42 +0800

    DB rename & update carbon

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 33060);
-define('DB_DATABASE', 'blog_0804');
+define('DB_DATABASE', 'wisd_0804_p');
 define('DB_CHARSET', 'utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
