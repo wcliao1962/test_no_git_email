4b9c4af50ed3e84b6e804b0eb20bb46e784285e6
Liao Wen Chung <>
2022-01-11 21:19:31 +0800

    test git email & name

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_PORT', 33060);
-define('DB_DATABASE', 'wisd_0804_p');
+define('DB_HOST', 'localhost:33060');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'lara_0804_p');
 define('DB_CHARSET', 'utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
